eee3e74af6617e0e40e50059daddee7ab5ebe1e8
Grant Holmes <g.holmes429@gmail.com>
2021-03-10 14:19:47 -0600

    added __init__.py to ui package

---
+++ core/ui/__init__.py
@@ -0,0 +1,25 @@
+"""
+Modules
+-------
+environments.py
+    Environment that modulates game logic with different settings.
+genetics.py
+    Uses multi-core parallel processing to train neural networks through natural selection.
+game.py
+    Exposes methods to play games, both hidden and rendered to GUI window.
+snakes.py
+    Snake objects that can play game in an environment.
+graphics.py
+    Pygame wrapper allowing for easy, convenient, and customizable GUI windows.
+settings.py
+    Global settings for game and program.
+constants.py
+    Constant variables to improve readability.
+neural_nets.py
+    Contains neural networks.
+behaviors.py
+    Contains behaviors for Snake that determine movement in response to Snake vision.
+"""
+
+__author__ = "Grant Holmes"
+__email__ = "g.holmes429@gmail.com"
